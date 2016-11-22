85ba71c61b969d6d07f152c3d793c5945fb7660b
claranobre <aclaranobre@gmail.com>
2016-11-22 10:16:48 -0300

    Fork do Projeto

---
+++ BatalhaNaval/src/ojogo/Evento.java
@@ -0,0 +1,18 @@
+package ojogo;
+
+/**
+ * Representa um evento do jogo
+ */
+
+public class Evento {
+   private String mensagem;
+
+   public Evento (String mensagem) {
+        this.mensagem = mensagem;   
+   }
+
+   public String getMensagem() {
+        return this.mensagem;
+   }
+}
+
---
+++ BatalhaNaval/src/ojogo/Principal.java
@@ -0,0 +1,21 @@
+package ojogo;
+
+import ojogo.gui.*;
+
+public class Principal {
+	/*
+	 * Versão do Jogo
+	 */
+	public static final float VERSAO = 1.1f;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Jogo novoJogo = new Jogo(Jogo.MEDIO);
+
+		JanelaPrincipal principal = new JanelaPrincipal(novoJogo);
+		principal.pack();
+		principal.setVisible(true);
+	}
+}
